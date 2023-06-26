b8b3af5a48328da4d58dd69e9e8dc8f6cb5abefc
zhaixiaojuan <zhaixiaojuan@loongson.cn>
2023-06-26 14:48:04 +0800

    Add libs vendor

---
+++ src/libs/protocols/vendor/ttrpc/src/asynchronous/mod.rs
@@ -0,0 +1,20 @@
+// Copyright (c) 2020 Ant Financial
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+//! Server and client in async mode (alias r#async).
+
+pub mod client;
+pub mod server;
+pub mod stream;
+#[macro_use]
+pub mod utils;
+mod unix_incoming;
+
+#[doc(inline)]
+pub use crate::r#async::client::Client;
+#[doc(inline)]
+pub use crate::r#async::server::Server;
+#[doc(inline)]
+pub use crate::r#async::utils::{convert_response_to_buf, MethodHandler, TtrpcContext};
