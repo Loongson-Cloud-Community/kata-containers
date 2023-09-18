84307b44923edff8ffff4c626f3282edac6a2944
zhaixiaojuan <zhaixiaojuan@loongson.cn>
2023-09-17 21:27:16 -0400

    Make kernel image

--- tools/packaging/static-build/kernel/build.sh
+++ tools/packaging/static-build/kernel/build.sh
@@ -15,22 +15,28 @@ readonly kernel_builder="${repo_root_dir}/tools/packaging/kernel/build-kernel.sh
 
 DESTDIR=${DESTDIR:-${PWD}}
 PREFIX=${PREFIX:-/opt/kata}
-container_image="kata-kernel-builder"
+container_image="kata-kernel-builder:4.19-7.4"
 
-sudo docker build -t "${container_image}" "${script_dir}"
+if [ "$(uname -m)" != "loongarch64" ]; then
+	sudo docker build -t "${container_image}" "${script_dir}"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	-w "${PWD}" \
-	"${container_image}" \
-	bash -c "${kernel_builder} $* setup"
+	sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+		-w "${PWD}" \
+		"${container_image}" \
+		bash -c "${kernel_builder} $* setup"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	-w "${PWD}" \
-	"${container_image}" \
-	bash -c "${kernel_builder} $* build"
+	sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+		-w "${PWD}" \
+		"${container_image}" \
+		bash -c "${kernel_builder} $* build"
 
-sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
-	-w "${PWD}" \
-	--env DESTDIR="${DESTDIR}" --env PREFIX="${PREFIX}" \
-	"${container_image}" \
-	bash -c "${kernel_builder} $* install"
+	sudo docker run --rm -i -v "${repo_root_dir}:${repo_root_dir}" \
+		-w "${PWD}" \
+		--env DESTDIR="${DESTDIR}" --env PREFIX="${PREFIX}" \
+		"${container_image}" \
+		bash -c "${kernel_builder} $* install"
+else
+	cp ${repo_root_dir}/tools/packaging/kernel/configs/loongarch_kata_kvm_4.19.x  ${script_dir}/loongarch_kata_kvm_4.19.x
+	sudo docker build --build-arg INSTALL_PATH="/opt/kata/share/kata-containers" -t "${container_image}" -f Dockerfile-loongarch64 "${script_dir}"
+	rm -rf ${script_dir}/loongarch_kata_kvm_4.19.x
+fi
